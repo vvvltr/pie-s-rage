74d72b2335170bb0d6948dc0f46b7abaf6dca0c1
wath23 <chipll2012@yandex.ru>
2022-07-06 13:55:26 +0300

    scripts separate addition

---
+++ Assets/Scripts/Hallway/TestMoveMous.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMoveMous : MonoBehaviour
+{
+    private int col;
+    private Vector3 pos;
+    public GameObject cheker;
+    public int w_or_h;
+    void Start()
+    {
+        pos = transform.position;
+        col = 0;
+    }
+    void Update()
+    {
+        if(col != 0)
+        {
+            transform.position = pos;
+            col--;
+        }
+        else
+        {
+            pos = transform.position;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.rigidbody)
+        {
+            //transform.position = pos;
+            cheker.GetComponent<Moves>().inCollision();
+            col = 2;
+        }
+        else
+        {
+            //pos = transform.position;
+        }
+    }
+    bool IsCollising()
+    {
+        return !(col == 0);
+    }
+}
