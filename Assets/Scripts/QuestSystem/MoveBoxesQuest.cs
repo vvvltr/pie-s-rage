2bef105b0ae456084d9fdbcd880753f61da2a9a5
Максим Родин <m.rodin@infomatika.ru>
2022-07-08 12:57:33 +0300

    small addition
    сохранение положения коробок и свет для избегания чёрных квадратов вместо картинок.

--- Assets/Scripts/QuestSystem/MoveBoxesQuest.cs
+++ Assets/Scripts/QuestSystem/MoveBoxesQuest.cs
@@ -30,6 +30,10 @@ namespace QuestSystem
         {
             questManager = GameObject.Find("QuestManager").GetComponent("QuestManager") as QuestManager;
             thisQuest = questManager.Quests[0];
+            if (questManager.CompletedQuests.Count > 0)
+            {
+                _camera.transform.GetComponent<EventController>().StopMiniGame();
+            }
         }
 
         private void Update()
